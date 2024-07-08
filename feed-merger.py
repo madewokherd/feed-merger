1c8c5d3f0b47ddf745db0897498d2d11c409d6dc
Esme Povirk <madewokherd@gmail.com>
2024-07-08 23:02:06 +0000

    More architecture/design. Static pages can now be checked for updates.

--- feed-merger.py
+++ feed-merger.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+import datetime
+import html
+import html.parser
+import json
 import os
 import os.path
 import readline
@@ -8,17 +12,103 @@ import traceback
 
 import core
 
-PRINT_JSON = True
+entry_template = """<h1><a name="item{e['fm:counter']}"></a><?if e.get('fm:link')><a href="{e['fm:link']}"><?endif>{' - '.join(x for x in (e.get('fm:feedname') or f.get('fm:title'), e.get('fm:author'), e.get('fm:title')) if x)}<?if e.get('fm:link')></a><?endif> {e['fm:timestamp']} <a href="#item{e['fm:counter']}">[anchor]</a></h1>
 
-descfilename = sys.argv[1]
-output_filename = sys.argv[2]
+<?if e.get('fm:html')><?html {translate_html(f, e, e['fm:html'])}><?endif>
+"""
 
-with open('feed-merger-state') as f:
-    state = eval(f.read())
+class HtmlTemplateFiller(html.parser.HTMLParser):
+    def __init__(self, locals_dict):
+        super().__init__()
+        self.locals_dict = locals_dict
+        self.strs = []
+        self.stack = []
+        self.output_enabled = True
 
-items = []
+    def get_contents(self):
+        return ''.join(self.strs)
 
-def process_line(line):
+    def handle_starttag(self, tag, attrs, close=False):
+        if not self.output_enabled:
+            return
+
+        self.strs.append('<')
+        self.strs.append(tag)
+
+        for (key, value) in attrs:
+            self.strs.append(' ')
+            self.strs.append(key)
+            self.strs.append('="')
+            self.strs.append(html.escape(str(eval(f'f"""{value}"""', globals(), self.locals_dict))))
+            self.strs.append('"')
+
+        if close:
+            self.strs.append('/>')
+        else:
+            self.strs.append('>')
+
+    def handle_endtag(self, tag):
+        if not self.output_enabled:
+            return
+
+        self.strs.append('</')
+        self.strs.append(tag)
+        self.strs.append('>')
+
+    def handle_startendtag(self, tag, attrs):
+        self.handle_starttag(tag, attrs, close=True)
+
+    def handle_data(self, data):
+        if not self.output_enabled:
+            return
+
+        self.strs.append(html.escape(str(eval(f'f"""{data}"""', globals(), self.locals_dict))))
+
+    def handle_pi(self, data):
+        if ' ' in data:
+            name, rest = data.split(' ', 1)
+        else:
+            name = data
+            rest = ''
+
+        if name == 'if':
+            self.stack.append(('if', self.output_enabled))
+            if self.output_enabled:
+                self.output_enabled = bool(eval(rest, globals(), self.locals_dict))
+        elif name == 'endif':
+            name, output_enabled = self.stack.pop()
+            if name != 'if':
+                raise Exception("template has <?endif> without <?if>")
+            self.output_enabled = output_enabled
+        elif name == 'html':
+            if not self.output_enabled:
+                return
+
+            self.strs.append(str(eval(f'f"""{rest}"""', locals=self.locals_dict)))
+        else:
+            raise Exception(f"template has unknown processing instruction {name}")
+
+def items_from_json(j, items):
+    if 'fm:entries' in j:
+        for entry in j['fm:entries']:
+            template_filler = HtmlTemplateFiller({
+                'e': entry,
+                'f': j,
+            })
+            template_filler.feed(entry_template)
+            template_filler.close()
+
+            items.append((template_filler.get_contents(), entry['fm:timestamp']))
+
+def translate_html(feed, entry, data):
+    # FIXME: handle base, tag filtering, and removal of <html> and <head> tags properly
+    if '<body' in data:
+        data = data.split('<body', 1)[1].split('>', 1)[-1]
+    if '</body>' in data:
+        data = data.split('</body>', 1)[0]
+    return data
+
+def handle_line(line):
     if line.startswith('mastodon:'):
         import mastodon
         mastodon.process(line, state, items)
@@ -55,56 +145,110 @@ def process_line(line):
         return web.process(line, state)
     elif line.endswith('.txt'):
         process_file(line)
+        return core.SUCCESS, None
     else:
         raise NotImplementedError
+
+item_counter = 0
+
+def add_defaults(line, j):
+    for entry in j['fm:entries']:
+        if 'fm:title' not in entry:
+            entry['fm:title'] = line
+        if 'fm:timestamp' not in entry:
+            entry['fm:timestamp'] = datetime.datetime.now(datetime.timezone.utc).isoformat()
+        if 'fm:id' not in entry and 'fm:link' in entry:
+            entry['fm:id'] = entry['fm:link']
+        global item_counter
+        item_counter += 1
+        entry['fm:counter'] = item_counter
+
+def process_line(line):
+    disposition, data = handle_line(line)
+    while disposition == core.REDIRECT:
+        disposition, data = handle_line(data)
+    if disposition == core.JSON:
+        add_defaults(line, data)
+        items_from_json(data, items)
+    elif disposition != core.SUCCESS:
+        raise Exception("unrecognized disposition")
 
 def process_file(descfilename):
     with open(descfilename) as f:
         for line in f:
             try:
                 line = line.strip()
-                disposition, data = process_line(line)
-                while disposition == core.REDIRECT:
-                    disposition, data = process_line(line)
-                if disposition == core.SUCCESS:
-                    continue
-                if disposition == core.JSON:
-                    json.dump(data, sys.stdout, indent=2)
-                else:
-                    raise Exception("unrecognized disposition")
+                process_line(line)
             except:
                 print("Failed processing line: ", line)
                 traceback.print_exc()
 
-process_line(descfilename)
+descfilename = sys.argv[1]
+output_filename = sys.argv[2]
 
-items.sort(key = lambda i: i[1])
+items = []
 
-with open(output_filename, 'w') as f:
-    f.write(
-    """<!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="utf-8">
-        <title>""")
+if output_filename == 'debug':
+    state = {}
 
-    f.write(descfilename)
+    line = descfilename
+    disposition, data = handle_line(line)
+    while disposition == core.REDIRECT:
+        print("redirected to", data)
+        line = data
+        disposition, data = handle_line(line)
 
-    f.write("""</title>
-      </head>
-      <body>
-    """)
+    if disposition == core.SUCCESS:
+        print("handler returned success")
+    elif disposition == core.JSON:
+        json.dump(data, sys.stdout, indent=2)
+        add_defaults(line, data)
+        print()
+        print()
+        items_from_json(data, items)
 
-    for (item, timestamp) in items:
-        f.write(item)
+    print("stored data:", state)
+    print()
 
-    f.write("</body></html>")
+    print('items:')
+    for item in items:
+        print(item[1], item[0])
+else:
+    try:
+        with open('feed-merger-state') as f:
+            state = eval(f.read())
+    except FileNotFoundError:
+        state = {}
 
-state_str = repr(state)
-eval(state_str)
+    process_line(descfilename)
 
-with open('feed-merger-state.new', 'w') as f:
-    f.write(state_str)
+    items.sort(key = lambda i: i[1])
 
-os.replace('feed-merger-state.new', 'feed-merger-state')
+    with open(output_filename, 'w') as f:
+        f.write(
+        """<!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="utf-8">
+            <title>""")
+
+        f.write(descfilename)
+
+        f.write("""</title>
+          </head>
+          <body>
+        """)
+
+        for (item, timestamp) in items:
+            f.write(item)
+
+        f.write("</body></html>")
+
+    state_str = repr(state)
+    eval(state_str)
+
+    with open('feed-merger-state.new', 'w') as f:
+        f.write(state_str)
+
+    os.replace('feed-merger-state.new', 'feed-merger-state')
 
--- web.py
+++ web.py
@@ -1,6 +1,109 @@
+import datetime
+import email.utils
 import html.parser
+import urllib.request
 
 import core
 
+# token values:
+STARTTAG = 'STARTTAG'
+ENDTAG = 'ENDTAG'
+DATA = 'DATA'
+COMMENT = 'COMMENT'
+DECL = 'DECL'
+PI = 'PI'
+UNKNOWN = 'UNKNOWN'
+
+class HtmlTokenizer(html.parser.HTMLParser):
+    def __init__(self, convert_charrefs=True):
+        self.tokens = []
+        super().__init__(convert_charrefs=convert_charrefs)
+
+    def handle_starttag(self, tag, attrs):
+        self.tokens.append((STARTTAG, tag, attrs))
+    
+    def handle_endtag(self, tag):
+        self.tokens.append((ENDTAG, tag, False))
+
+    def handle_startendtag(self, tag, attrs):
+        self.tokens.append((STARTTAG, tag, attrs))
+        self.tokens.append((ENDTAG, tag, True))
+
+    def handle_data(self, data):
+        self.tokens.append((DATA, data, None))
+
+    def handle_comment(self, data):
+        self.tokens.append((DATA, data, None))
+
+    def handle_decl(self, decl):
+        self.tokens.append((DECL, decl, None))
+
+    def handle_pi(self, data):
+        self.tokens.append((PI, data, None))
+
+def fetch_http(url, data=None, headers=None):
+    # returns json, headers, response
+    req = urllib.request.Request(url, data=data, headers=headers)
+    response = urllib.request.urlopen(req)
+
+    if response.status == 304:
+        return None, response.headers, response
+
+    j = {}
+    # fill in http fields
+    if 'http:headers' not in j:
+        j['http:headers'] = dict(response.headers)
+    if 'http:url' not in j:
+        j['http:url'] = url
+
+    return j, response.headers, response
+
+def fill_http_defaults(json):
+    j = json
+    if 'fm:base' not in j and 'http:url' in j:
+        j['fm:base'] = j['http:url']
+
+    if 'http:headers' in j and "Last-Modified" in j['http:headers']:
+        mtime_iso = email.utils.parsedate_to_datetime(j['http:headers']['Last-Modified']).isoformat()
+
+        if 'http:mtime_iso' not in j:
+            j['http:mtime_iso'] = mtime_iso
+
+        if 'fm:entries' in j:
+            for entry in j['fm:entries']:
+                if 'fm:timestamp' not in entry:
+                    entry['fm:timestamp'] = mtime_iso
+
+    if 'fm:entries' in j and 'http:url' in j:
+        for entry in j['fm:entries']:
+            if 'fm:link' not in entry:
+                entry['fm:link'] = j['http:url']
+
 def process(line, state):
-    return core.SUCCESS, None
+    etag = state.get(('web', line, 'etag'))
+    mtime = state.get(('web', line, 'mtime'))
+
+    headers = {}
+    if etag:
+        headers['If-None-Match'] = etag
+    if mtime:
+        headers['If-Modified-Since'] = mtime
+
+    json, headers, response = fetch_http(line, headers=headers)
+    if json is None:
+        # 304 status
+        return core.SUCCESS, None
+
+    # Don't know how to parse this, just use the defaults
+    result = core.JSON, json
+    json['fm:entries'] = [{}]
+
+    if result[0] == core.JSON:
+        j = result[1]
+
+        fill_http_defaults(j)
+
+    state['web', line, 'etag'] = response.headers.get('ETag')
+    state['web', line, 'mtime'] = response.headers.get('Last-Modified')
+
+    return result
